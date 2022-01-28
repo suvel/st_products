eed3727d3b6b3adeeb36131b0cab865a465ba81f
Suvel Rathneswar <find.suvel@gmail.com>
2022-01-28 06:54:07 +0530

    Added random products

--- script.js
+++ script.js
@@ -1,3 +1,250 @@
+const prods = [
+  {
+    name: "Dish_wash",
+    price: "40.00",
+    img: "http://sargurutech.org/online/images/dishwas.jpg",
+  },
+  {
+    name: "Dish_wash_pro",
+    price: "12.00",
+    img: "http://sargurutech.org/online/images/dishwas1.jpg",
+  },
+  {
+    name: "Detergent_liquid",
+    price: "100.00",
+    img: "http://sargurutech.org/online/images/mulcl1.jpg",
+  },
+  {
+    name: "Wash_akk",
+    price: "15.00",
+    img: "http://sargurutech.org/online/images/mwliq.jpg",
+  },
+  {
+    name: "Medicated_soap",
+    price: "20.00",
+    img: "http://sargurutech.org/online/images/soapoil.jpg",
+  },
+  {
+    name: "Wash_Natural",
+    price: "25.00",
+    img: "http://sargurutech.org/online/images/flocl.jpg",
+  },
+  {
+    name: "Hygienic_item",
+    price: "25.00",
+    img: "http://sargurutech.org/online/guruimg/pic1.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic2.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic3.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic4.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic5.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic6.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic7.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic8.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic9.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic10.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic11.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic12.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic13.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic14.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic15.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic16.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic17.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic18.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic19.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic20.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic21.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic22.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic23.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic24.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic25.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic26.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic27.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic28.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic29.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic30.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic31.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic32.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic33.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic34.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic35.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic36.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic37.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic38.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic39.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic40.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic41.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic42.jpg",
+  },
+  {
+    name: "Higineic_item",
+    price: "100.00",
+    img: "http://sargurutech.org/online/guruimg/pic43.jpg",
+  },
+];
 let menuBar = document.getElementById("menu-bar");
 
 let isMenuOpened = false;
@@ -12,4 +259,27 @@ function toggleMenuBar() {
   if (!isMenuOpened) {
     sideBar.style.display = "none";
   }
+}
+
+window.addEventListener("load", handelOnWindowLoad);
+
+function createProduct(src, name, price) {
+  return `<div class="product">
+  <div class="product_image">
+    <img src="${src}" />
+  </div>
+  <div class="product_name">${name}</div>
+  <div class="product_price">RS.${price}</div>
+  <button class="add_to_cart">Add to cart</button>
+  </div>`;
+}
+
+function handelOnWindowLoad() {
+  const products = document.getElementById("products");
+  productsHtmlString = prods.reduce(function (acc, curVal) {
+    const prdHMLStr = createProduct(curVal.img, curVal.name, curVal.price);
+    return acc + prdHMLStr;
+  }, "");
+  debugger
+  products.innerHTML = productsHtmlString;
 }
